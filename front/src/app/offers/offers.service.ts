a74a57a335384b0436d305d16d05b44a98005248
Riaeth <drasneel@gmail.com>
2019-05-24 10:55:18 +0200

    Service Completado

--- front/src/app/offers/offers.service.ts
+++ front/src/app/offers/offers.service.ts
@@ -4,7 +4,7 @@ import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
-import { Owner } from 'app/owners/owner';
+import { Offer } from './offer';
 
 
 
@@ -20,11 +20,49 @@ export class OffersService {
   constructor(private http: HttpClient, private httpErrorHandler: HttpErrorHandler) {
     this.handlerError = httpErrorHandler.createHandleError('OwnerService');
   }
-  
-  getOffers(): Observable<Owner[]> {
-    return this.http.get<Owner[]>(this.entity_url)
+
+  getOffers(): Observable<Offer[]> {
+    return this.http.get<Offer[]>(this.entity_url)
       .pipe(
-        catchError(this.handlerError('getOwners', []))
+        catchError(this.handlerError('getOffers', []))
       );
   }
+
+  
+  getOfferById(offer_id: string): Observable<Offer> {
+    return this.http.get<Offer>(this.entity_url + '/' + offer_id)
+      .pipe(
+          catchError(this.handlerError('getOfferById', {} as Offer))
+      );
+  }
+
+  addOffer(offer: Offer): Observable<Offer> {
+    return this.http.post<Offer>(this.entity_url, offer)
+      .pipe(
+        catchError(this.handlerError('addOffer', offer))
+      );
+  }
+
+  updateOffer(offer_id: string, offer: Offer): Observable<{}> {
+    return this.http.put<Offer>(this.entity_url + '/' + offer_id, offer)
+      .pipe(
+        catchError(this.handlerError('updateOffer', offer))
+      );
+  }
+
+  deleteOffer(offer_id: string): Observable<{}> {
+    return this.http.delete<Offer>(this.entity_url + '/' + offer_id)
+      .pipe(
+         catchError(this.handlerError('deleteOffer', [offer_id]))
+      );
+  }
+
+  getOffersValid(): Observable<Offer[]> {
+    return this.http.get<Offer[]>(this.entity_url+ '/valid')
+      .pipe(
+        catchError(this.handlerError('getOffersValid', []))
+      );
+  }
+
+
 }
