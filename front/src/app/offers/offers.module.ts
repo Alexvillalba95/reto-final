b449fbb8b225f37dfe231e047278cace68469d21
Alexvillalba95 <alexvillalba95@gmail.com>
2019-05-24 10:27:07 +0200

    Offers-list component

--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OfferListComponent } from './offers-list/offer-list/offer-list.component';
+import { OffersListComponent } from './offers-list/offers-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [OfferListComponent, OffersListComponent]
 })
 export class OffersModule { }
