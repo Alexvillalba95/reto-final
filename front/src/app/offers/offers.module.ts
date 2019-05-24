943b28f4213fae865db10e81f0ef8cc1127b3691
ldat95 <olmedo.antonio95@gmail.com>
2019-05-24 10:27:50 +0200

    creacion form-offer

--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormOfferComponent } from './form-offer/form-offer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FormOfferComponent]
 })
 export class OffersModule { }
