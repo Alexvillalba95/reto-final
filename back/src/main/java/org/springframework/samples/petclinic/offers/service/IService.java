643770d052572dc18d0e8d7766cb87100e25720c
Otakora <ronsistemas>
2019-05-24 12:14:14 +0200

    Service v2

--- back/src/main/java/org/springframework/samples/petclinic/offers/service/IService.java
+++ back/src/main/java/org/springframework/samples/petclinic/offers/service/IService.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.OfferEntity;
-import org.springframework.samples.petclinic.model.Owner;
+
 
 public interface IService {
 	OfferEntity findOfferById(int id) throws DataAccessException;
