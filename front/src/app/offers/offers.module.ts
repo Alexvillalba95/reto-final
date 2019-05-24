c398d4476b49d12538571993233c3fed71c98cb1
jmsuarez93 <jmsuarez93@hotmail.com>
2019-05-24 10:30:14 +0200

    Creacion de offerHome

--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OfferListComponent } from './offers-list/offer-list/offer-list.component';
 import { OffersListComponent } from './offers-list/offers-list.component';
+import { OfferHomeComponent } from './offer-home/offer-home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [OfferListComponent, OffersListComponent]
+  declarations: [OfferListComponent, OffersListComponent, OfferHomeComponent]
 })
 export class OffersModule { }
