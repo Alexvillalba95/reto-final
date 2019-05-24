4e2a6ec844cfe15a9a788ccf810f4299a4d6bd28
Riaeth <drasneel@gmail.com>
2019-05-24 11:25:08 +0200

    Routing añadido

---
+++ front/src/app/offers/offers-routing.module.ts
@@ -0,0 +1,21 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OffersListComponent } from './offers-list/offers-list.component';
+import { FormOfferComponent } from './form-offer/form-offer.component';
+import { OfferHomeComponent } from './offer-home/offer-home.component';
+
+const offerRoutes: Routes = [
+  { path: 'offers', component: OfferHomeComponent },
+  { path: 'offers/add', component: FormOfferComponent },
+  { path: 'offers/list', component: OffersListComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(offerRoutes)],
+  exports: [RouterModule]
+})
+
+export class OffersRoutingModule {
+}
