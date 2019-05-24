3555f9aab859bb6c3fc69365a3bf449bbcc4e875
Otakora <ronsistemas>
2019-05-24 12:29:17 +0200

    Service v2.2

--- back/src/main/java/org/springframework/samples/petclinic/offers/service/IService.java
+++ back/src/main/java/org/springframework/samples/petclinic/offers/service/IService.java
@@ -4,8 +4,12 @@ import java.util.Collection;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.OfferEntity;
+import org.springframework.samples.petclinic.model.Pet;
 
 public interface IService {
 	OfferEntity findOfferById(int id) throws DataAccessException;
 	Collection<OfferEntity> findAllOffer() throws DataAccessException;
+	void createOffer(OfferEntity offer);
+	void deleteOffer(OfferEntity offer);
+	void updateOffer(OfferEntity offer);
 }
